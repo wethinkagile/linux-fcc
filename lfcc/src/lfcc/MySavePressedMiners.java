2ff4f80b620ff778507d7c90eda206891232c4cd
Stephan Kristyn <nottinhill@ecomail.at>
2011-08-02 03:38:30 +0200

    Added About Dialog Box
    	modified:   lfcc/src/lfcc/MyChangeAction.java
    	modified:   lfcc/src/lfcc/MyCommitPressed.java
    	modified:   lfcc/src/lfcc/MyCommitPressedClocks.java
    	modified:   lfcc/src/lfcc/MySavePressedMiners.java
    	modified:   lfcc/src/lfcc/RegExAtiConfigOc.java
    	modified:   lfcc/src/lfcc/RegExAtiConfigTemp.java
    	modified:   lfcc/src/lfcc/WindowContainer.java

--- lfcc/src/lfcc/MySavePressedMiners.java
+++ lfcc/src/lfcc/MySavePressedMiners.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lfcc;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
